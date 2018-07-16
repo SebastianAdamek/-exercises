2c783d78f1d46f92eed1b428fdaf750925819fe9
sebadamek <sebadamek@wp.pl>
2018-07-16 07:30:18 +0200

    add idnex.php to project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>New Project</title>
+	</head>
+	<body>
+		<h1>Hello World</h1>
+		<p>It's my new project, where a learned how to use Python and other progs(git, pip, others)</p>
+	</body>
+</html>
